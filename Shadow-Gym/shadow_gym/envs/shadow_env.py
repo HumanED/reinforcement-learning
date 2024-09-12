dda54be80cb60d5e879ecc488b7a440c8e7d2b5e
Ethan Cheam <ethan.cheam123@gmail.com>
2024-09-12 11:06:42 +0100

    Fixed bug in self.info

--- Shadow-Gym/shadow_gym/envs/shadow_env.py
+++ Shadow-Gym/shadow_gym/envs/shadow_env.py
@@ -240,15 +240,15 @@ class ShadowEnv(gymnasium.Env):
                                     random.randint(-1,1) * np.pi/2)
         random_orientation_q = p.getQuaternionFromEuler(random_orientation_euler)
         self.cube = Cube(self.client, random_orientation_q)
-        # 20 random actions
-        for _ in range(20):
-            action = self.action_space.sample()
-            self.step(action)
 
         self.terminated = False
         self.truncated = False
         self.num_steps = 0
         self.info = {}
+        # 20 random actions
+        for _ in range(20):
+            action = self.action_space.sample()
+            self.step(action)
         self.info["ep_steps"] = self.num_steps
         self.info["success"] = False
 
