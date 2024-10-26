9f0ff5437b47206fdad48c4399bec91b3fbaec77
pmcgrath735 <s2483423@ed.ac.uk>
2024-10-26 12:33:52 +0100

    added two tests

--- visualise_model.py
+++ visualise_model.py
@@ -13,8 +13,8 @@ normalized_env = False
 # Set num_evaluate to -1 to enable rendering and just view the project
 num_evaluate = -1
 # Run name should have model, unique number, and optionally a description
-run_name = "PPO-19-shadowgym-ethan"
-model_file = "700000.zip"
+run_name = "PPO-02-shadowgym-peter"
+model_file = "2375000.zip"
 normalize_stats_file = "1800000.pkl"
 
 
